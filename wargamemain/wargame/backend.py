b2ddb20773b343f13ee56fd2ff0983b45f56d23f
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-09-28 15:52:08 +0200

    fixed git, adding file correctly

---
+++ wargamemain/wargame/backend.py
@@ -0,0 +1,52 @@
+
+
+list_of_questions = ['Do Twojej wioski ma  przypłynąć poselstwo ze stolicy. Oznacza to liczne rozmowy i poznawanie nowych ludzi. Organizujesz więc  festiwal Rzepy, który nie może się odbyć bez Twojego PEŁNEGO zaangażowania. Twój zastępca ogarnie delegacje. Rzepa to ważna rzecz! '
+                     'Kiedy naprawiałeś łódź razem ze swoim przyjacielem, zwróciłeś uwagę na małe stworzonko, które dreptało po maszcie. Zamyśliłeś się. Kim my jesteśmy we wszechświecie? Jaki jest nasz cel? Wzniosłeś się na nowe wyżyny percepcji. W tym samym czasie, na Rollo spadł maszt, bo puściłeś linę, która go utrzymywała w miejscu.  Wódz ma prawo czasem zatracić się w myślach.'
+                     'Jako, że jesteś najlepszy wodzem od wielu  dziesięcioleci, zajmujesz się również sprawą listów o tematyce wojskowej. Nie umiesz czytać, więc każdy list, trzymasz razem z posłańcem, który go przyniósł. Dokładnie ich dzielisz: na tych, których wypuścisz, tych których nie wypuścisz i tych co do których jeszcze ciągle się wahasz. Porządek musi być! '
+                     'W czasie ostatniej długiej podróży zaatakował Was Kraken. Częśc marynarzy wpadło w panikę, ale Ty po prostu poszedłeś spać. Niewielkie kryzysowe sytuacje nie zaburzają Twojego „trzeźwego umysłu”, potrafisz odpocząć i zrelaksować się. '
+                     '''„Hmpf”...... „mmmm”.... „Hmpf”... \n Tak mniej więcej wyglądają narady wojenne w Waszej wiosce. Żaden wiking nie lubi inicjować  rozmów, więc nikt się pierwszy nie odzywa. Narady trwają wieczność, ale Ty musisz pozostać wierny tradycji...'''
+                     'Twoja załoga odkryła dziwny owoc na jednej z wysp. Nikt nie wie jak smakuje. Dla bezpieczeństwa nie jesz go sam, tylko każesz go spróbować swojemu najmniej lubianemu wojowi. Bezpieczeństwo ważniejsze od ciekawości. '
+                     'Na ostatnim spotkaniu Stowarzyszenia Wodzów Małych Wiosek, padł pomysł wprowadzenia okrągłego stołu – tak żeby nikt się nie czuł urażony. Co za bzdura. Oczywistym jest, że jedni wodzowie są lepsi od innych. Na przykład Ty'
+                     'Przed wyruszeniem w daleki rejs wraz z załogą dokładnie określasz gdzie przystaniecie na kawę oraz, w którym momencie zrobić przerwę na plądrowanie. Wszystko musi być dokładnie ustalone! Nie możemy sobie pozwolić w tej kwestii na improwizację.'
+                     'Otrzymałeś ostatnio tytuł najbardziej zmotywowanego i energicznego grabieżcy. Co prawda usunąłeś wszystkich swoich konkurentów, ale ten tytuł i tak Ci się należał prawda?'
+                     'Władca Twojego królestwa uważa, że dostarczas zbyt małą ilość złota do królewskiego skarbca.  Prowadzicie długą dyskusję na ten temat, jednak w końcu ustępujesz, bo relacja z Twoim władcą jest dla Ciebie ważniejsza. Poza tym, zagroził, że nabije Cię na pal...'
+                     'Olbrzym Hugi leży u Twych stóp.  Prawdę powiedziawszy pokonałeś go przez przypadek, bo on potknął się o własną nogę. Twoi wojowie wychwalają Twoje imię, ale Ty organizujesz konferencję prasową, aby wyjaśnić wszystkie niejasności.'
+                     'Odwiedzasz swojego dawnego sojusznika w jego osadzie. Nie czujesz się jednak komfortowo w jego komnatach - są bardzo nieporządne. Nie może być tak, że czaski pokonanych wrogów, leżą zaraz obok zrabowanych skarbów. Wolisz jak wszystko jest zorganizowane i uporządkowane - tak jak u Ciebie.'
+                     'Podczas corocznej uroczystości składania czci bóstwom, wygłaszasz płomienną mowę na temat „Istnienie bogów a zasługi naszego wodza”. Wszyscy słuchają Cię z zapałem, bo inaczej wygnasz ich z osady. '
+                     'Musisz zaprojektować nowy wygląd bandery Twojego Drakara. W końcu decydujesz się na brązowy prostokąt.  Prostackie? Gdzie tam. Miałeś akurat jedne takie nieużywane spodnie.  Projektowanie jest dla bab, a Ty w zaoszczędzonym czasie możesz plądrować. '
+                     'Ostatnimi czasy na Twojej łódce wybuchają niewielkie konflikty, ale Ty jesteś oazą spokoju. Bez cienia gniewu wyrzucasz buntowników za burtę. Niełatwo wyprowadzić Cię z równowagi...'
+                     'Twój jeniec mówi Ci „mmmmmskmf”. Często zdarza Ci się nie rozumieć reakcji ludzi, którzy Cię otaczają. Ich emocje są dla Ciebie tajemnicą nie do odkrycia. Może trzeba im wyjąć knebel z ust?'
+                     'Miewasz wahania nastrojów. Czasami bawią Cię pojedynki kasztanowych ludzików, a innym razem nawet pokonanie potwor a morskiego nie przynosi Ci radości...'
+                     'KIM BYŁA MOJA MATKA? – Krzyczy Jorgmilr. Hmm może mogłeś się powstrzymać od tego ostatniego komentarza, ale w sumie dotarcie do opisu prawdziwego stanu rzeczy powinno być zawsze ważniejsze od odczuć innych ludzi. '
+                     'W czasie rejsu pokłóciłeś się z Twoim nawigatorem i wyrzuciłeś go za burtę. To był Twój ostatni nawigator. Trudno.  Dasz sobie radę. Jesteś znany z tego, że nie przemjmujesz się skutkami swoich działań. '
+                     'Na uroczystości z okazji czerwonej Pełni Księżyca przyznane zostały ordery za największe zasługi. Wyróżnienie zostało przyznane twojemu zastępcy, a nie Tobie. W sytuacjach takich jak ta, najczęsciej czujesz zazdrość. On już nic nie czuje. Wyrzuciłeś go za burtę. '
+                     'Ciekawa, stara kronika albo festiwal Hod owców Rzepy  jest dla Ciebie często lepszą alternatywą spędzenia czasu niż spotkanie towarzyskie. '
+                     'Gdy przygotowujesz się do walki z olbrzymem to najważniejszą dla Ciebie kwestią jest stworzenie szczegółowo dopracowanego planu i późniejsze trzymanie się go. Twoi wojowie z porzodu, a Ty kilka kilometrów za nimi. '
+                     'Podczas Twoich spotkań z władcą, na których omawiane są plany dotyczące dalszego zdobywania terenów, musisz ciągle sprowadzać władcę na ziemię - uwielbia fantazjować i często daje się ponieść nierealnym ideom. Tobie zdarza się to bardzo rzadko. Poza tym ta osada, którą chciał spalić, to Twoja osada...'
+
+                     ]
+
+list_of_answers = ['Aboslutnie nie', 'Raczej nie', 'Raczej tak', 'Absolutnie tak']
+
+
+class Character(object):
+    total = 0
+    personality = ''
+
+    def __init__(self):
+        self.total = 0
+        self.personality = ''
+
+    def calculate_answers(self, answers, type):
+        self.total = 0
+        for answer in answers:
+            self.total += int(answer)
+        if self.total <= 12:
+            self.personality = self.personality + type[0]
+            return self.personality
+        elif self.total >= 13:
+            self.personality = self.personality + type[1]
+            return self.personality
+
+    def __repr__(self):
+        return f"{self.personality}"
